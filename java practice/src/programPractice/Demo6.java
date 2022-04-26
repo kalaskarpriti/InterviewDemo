ebaca7e1a43ee7b068f503416bb0187dc0234372
pritiQA <pritikalaskar92@gmail.com>
2022-04-26 10:17:13 +0530

    Prog added for digit

--- java practice/src/programPractice/Demo6.java
+++ java practice/src/programPractice/Demo6.java
@@ -3,7 +3,7 @@ package programPractice;
 public class Demo6 {
 
 	public static void main(String[] args) {
-		String str="test 4 test 5 tets 8 test 9trt 4 tests5";
+		String str="test 4 test 5 tets 8 test 9trt 4 test55 tests5";
 		char[] myChar=str.toCharArray();
 		StringBuilder builder=new StringBuilder();
 		
